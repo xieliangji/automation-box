0bd0a0a0ae20441b01424506eff62d31cb16f5ff
xieliangji <450324623@qq.com>
2023-05-20 18:06:25 +0800

    [IMP] - playwright quick started PlaywrightRunner

--- src/main/java/xieliangji/automation/playwright/PlaywrightRunner.java
+++ src/main/java/xieliangji/automation/playwright/PlaywrightRunner.java
@@ -1,8 +1,19 @@
 package xieliangji.automation.playwright;
 
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.assertions.PlaywrightAssertions;
+
 public class PlaywrightRunner {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws InterruptedException {
+        try (Playwright playwright = Playwright.create();
+             Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+             BrowserContext context = browser.newContext();
+             Page page = context.newPage();
+        ){
+            page.navigate("https://google.com/ncr");
+            PlaywrightAssertions.assertThat(page).hasTitle("Google");
+            Thread.sleep(2000);
+        }
     }
 }
